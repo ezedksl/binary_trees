befe67244ee045bf3159e68b1f9d2b8e8d0041f2
S <ezedksl@gmail.com>
2021-03-10 20:42:16 -0300

    update

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -11,14 +11,14 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 
 	if (node->parent->left != node)
 	{
-		if (!node->parent->right)
-			return (NULL);
-		return (node->parent->right);
-	}
-	else
-	{
 		if (!node->parent->left)
 			return (NULL);
 		return (node->parent->left);
+	}
+	else
+	{
+		if (!node->parent->right)
+			return (NULL);
+		return (node->parent->right);
 	}
 }
