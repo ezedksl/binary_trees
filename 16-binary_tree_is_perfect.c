d8d069f2d19af20c705abbc7d85e91c752198ab6
S <ezedksl@gmail.com>
2021-03-10 19:36:30 -0300

    update

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -14,3 +14,23 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	return (0);
 }
+
+/**
+ * binary_tree_size - Counts the total number of nodes of a binary tree
+ * @tree: Root node of the binary tree
+ * Return: Size of the tree || 0 if @tree NULL
+ */
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t count = 0;
+
+	if (!tree)
+		return (0);
+	if (!tree->left && !tree->right)
+		return (1);
+	if (tree->left)
+		count += binary_tree_size(tree->left);
+	if (tree->right)
+		count += binary_tree_size(tree->right);
+	return (count + 1);
+}
